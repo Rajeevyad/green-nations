d29268992215dceb30f054704b0ea0bfd1666ce3
ryadav46 <ryadav46@csc.com>
2018-06-07 12:09:47 +0530

    Status bar, top bar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import {FwModule} from '../fw/fw.module';
+import { FrameworkConfigService } from '../fw/services/framework-config.service';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,7 @@ import {FwModule} from '../fw/fw.module';
     BrowserModule,
     FwModule
   ],
-  providers: [],
+  providers: [FrameworkConfigService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
