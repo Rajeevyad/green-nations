aa620dd1252da9584df4fecff43aa4fe728aa8b9
ryadav46 <ryadav46@csc.com>
2018-06-08 11:31:06 +0530

    Menu Service added with Menu Item & their routing (Dashboard, Settings, Counties menu added)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 
 import { FrameworkConfigService, FrameworkConfigSettings } from '../fw/services/framework-config.service';
+import { MenuService } from '../fw/services/menu.service';
+import { initialMenuItems } from './app.menu';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor (private frameworkConfigService: FrameworkConfigService) {
+  
+  constructor (private frameworkConfigService: FrameworkConfigService,
+               private menuService: MenuService) {
 
     let config:FrameworkConfigSettings = {
       socialIcons: [
@@ -22,6 +27,8 @@ export class AppComponent {
     };
 
     frameworkConfigService.configure(config);
+
+    menuService.items = initialMenuItems;
 
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-
-import {FwModule} from '../fw/fw.module';
-import { FrameworkConfigService } from '../fw/services/framework-config.service';
-import { ScreenService } from '../fw/services/screen.service';
+import { FwModule } from '../fw/fw.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CountriesComponent } from './countries/countries.component';
+import { SettingsComponent } from './settings/settings.component';
+import { appRoutes } from './app.routing';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    CountriesComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
-    FwModule
+    FormsModule,
+    HttpModule,
+    FwModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [FrameworkConfigService, ScreenService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
